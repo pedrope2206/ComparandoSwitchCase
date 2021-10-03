ad019c757a5fc8f8dd7263460d4b79e6eea3d433
pedrope2206 <pivettapedro93@gmail.com>
2021-10-03 08:19:25 -0300

    ComparandoSwitchCase

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace ComparandoSwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int mes;
+
+            Console.WriteLine("Olá qual é seu nome?");
+            nome = Console.ReadLine();
+
+            Console.WriteLine($"Muito prazer, {nome}, escreva o número do mês a ser verificado");
+            mes = Convert.ToInt16(Console.ReadLine());
+
+            Switch(mes)
+            {
+                case1: 
+                Console.WriteLine("Este mes possui 31 dia");
+                break;
+
+                case2: 
+                Console.WriteLine("Este mes possui 28 ou 29 dia");
+                break;
+
+                case3: 
+                Console.WriteLine("Este mes possui 31 dia");
+                break;
+
+                case4: 
+                Console.WriteLine("Este mes possui 30 dia");
+                break;
+
+                case5: 
+                Console.WriteLine("Este mes possui 31 dia");
+                break;
+
+                case6: 
+                Console.WriteLine("Este mes possui 30 dia");
+                break;
+
+                case7: 
+                Console.WriteLine("Este mes possui 31 dia");
+                break;
+
+                case8: 
+                Console.WriteLine("Este mes possui 31 dia");
+                break;
+
+                case9: 
+                Console.WriteLine("Este mes possui 30 dia");
+                break;
+
+                case10: 
+                Console.WriteLine("Este mes possui 31 dia");
+                break;
+
+                case11: 
+                Console.WriteLine("Este mes possui 30 dia");
+                break;
+
+                case12: 
+                Console.WriteLine("Este mes possui 31 dia");
+                break;
+            }
+
+        }   
+    }
+}
